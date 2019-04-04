a0a3084f54a91b3ef5db74c2a3af56e474fca336
石光辉 <shiguanghui@gtexpress.cn>
2019-04-04 13:10:15 +0800

    branch_2 增加了Branch_2Test类

---
+++ CAShaperLayerDemo/Branch_2Test.h
@@ -0,0 +1,22 @@
+//
+//  Branch_2Test.h
+//  CAShaperLayerDemo
+//
+//  Created by jiahao on 2019/4/4.
+//  Copyright © 2019 SILICN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Branch_2Test : NSObject
+
+@property (nonatomic, copy)NSString *name;
+
+@property (nonatomic, assign)NSInteger age;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
